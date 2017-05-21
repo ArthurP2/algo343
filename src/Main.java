c4f797ad818a80b2f67c2f0a93623af3e00a35c5
ExGenere <panlilioarthur@yahoo.com>
2017-05-21 11:18:58 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Marky on 5/21/2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        Random2DArray r = new Random2DArray();
+    }
+
+}
---
+++ src/Random2DArray.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+/**
+ * Created by Marky on 5/21/2017.
+ */
+public class Random2DArray {
+
+    public Random2DArray(){
+
+        Random rand = new Random();
+
+        int n = 5;
+
+        int[][] random = new int[n][n];
+        for (int ii = 0; ii < n; ii++){
+            int inc = 0;
+            for (int qq = 0; qq < n; qq++){
+                int x = rand.nextInt(9) + 1 + inc;
+                random[ii][qq] = x;
+                if (ii >= qq){
+                    random[ii][qq] = 0;
+                } else {
+                    inc = x + rand.nextInt(3) + 1;
+                }
+                System.out.print(random[ii][qq] + "|");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
